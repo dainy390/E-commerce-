fa53a2bb7f28b851d2122c493f4eb490a27f2284
Dainy <112776500+dainy390@users.noreply.github.com>
2024-11-22 17:38:29 +0530

    Add dispatch to the dependency array.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import Nav from "./Component/Nav";
-// import ProductDetail from "./Component/ProductDetail";
 import ProductDetail from "./Component/ProductDetail";
 import AddProduct from "./Component/AddProduct";
 import CartItems from "./Component/CartItems";
-// import ProductItemList from "./Component/ProductItemList";
 import ProductItemList from "./Component/ProductItemList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { addproducts } from "./actions/index";
 import customFetch from "./apiCall";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 function App() {
-  let productDetailItem = useSelector((state) => state.itemToDisplay);
+  const productDetailItem = useSelector((state) => state.itemToDisplay);
+  const dispatch = useDispatch();
 
   const url = "https://my-json-server.typicode.com/jaiswalaryan/data/db";
 
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    let response = customFetch(url, {
-      method: "GET",
-    });
-    response.then((data) => {
-      let modifiedData = data.products.map((item) => {
+  // Use useCallback to stabilize the fetchData function
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await customFetch(url, { method: "GET" });
+      const modifiedData = response.products.map((item) => {
         item.edit = true;
         return item;
       });
+
       window.localStorage.setItem("products", JSON.stringify(modifiedData));
-      let products = JSON.parse(window.localStorage.getItem("products"));
+      const products = JSON.parse(window.localStorage.getItem("products"));
       dispatch(addproducts(products));
-    });
-  }, []);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  }, [url, dispatch]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="App">
